eb300c334bcb363811a41d899cbd28ad94e6ec79
FHU-yezi <yehaowei20060411@qq.com>
2022-07-08 09:13:56 +0800

    feat: 增加日志管理、数据库管理、配置管理模块

---
+++ db_manager.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+
+from config_manager import config
+
+
+def InitDB():
+    connection: MongoClient = MongoClient(config["db_address"], config["db_port"])
+    db = connection.JMonitorData
+    return db
+
+
+def GetCollection(name: str):
+    return db[name]
+
+
+db = InitDB()
+
+log_db = db.log
