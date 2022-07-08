4fe1eacd332d227193bab8eaaa8678e506910f41
FHU-yezi <yehaowei20060411@qq.com>
2022-07-08 15:55:58 +0800

    feat: 增加文章获钻量排行榜和大转盘抽奖数据采集任务

--- fetchers/__init__.py
+++ fetchers/__init__.py
@@ -1,7 +1,9 @@
-from fetchers import article_FP_rank, assets_rank, daily_update_rank
+from fetchers import (article_FP_rank, assets_rank, daily_update_rank,
+                      lottery_data)
 
 
 def init_tasks():
     article_FP_rank
     assets_rank
     daily_update_rank
+    lottery_data
