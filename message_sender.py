34acc852fa0a0d2edd3c7c4b1c5616d0ef7b3528
FHU-yezi <yehaowei20060411@qq.com>
2022-07-08 09:53:33 +0800

    feat: 加入消息发送模块

---
+++ message_sender.py
@@ -0,0 +1,94 @@
+from typing import Dict
+
+from httpx import post as httpx_post
+
+from config_manager import config
+from log_manager import AddRunLog
+from utils import CostSecondsToString, GetNowWithoutMileseconds
+
+
+def GetFeishuToken() -> str:
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    data = {
+        "app_id": config["message_sender/app_id"],
+        "app_secret": config["message_sender/app_secret"]
+    }
+    response = httpx_post("https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal",
+                          headers=headers, json=data)
+    if response.json()["code"] == 0:
+        return "Bearer " + response.json()["tenant_access_token"]
+    else:
+        AddRunLog("SENDER", "ERROR", f"获取 Token 时发生错误，错误码：{response.json()['code']}，错误信息：{response.json()['msg']}")
+        raise ValueError(f"获取 Token 时发生错误，错误码：{response.json()['code']}，错误信息：{response.json()['msg']}")
+
+
+def SendFeishuCard(card: Dict) -> None:
+    token = GetFeishuToken()
+    headers = {"Content-Type": "application/json; charset=utf-8",
+               "Authorization": token}
+
+    data = {
+        "email": config["message_sender/email"],
+        "msg_type": "interactive",
+        "card": card
+    }
+    response = httpx_post("https://open.feishu.cn/open-apis/message/v4/send/",
+                          headers=headers, json=data)
+    if response.json()["code"] != 0:
+        AddRunLog("SENDER", "ERROR", f"发送消息卡片时发生错误，错误码：{response.json()['code']}，错误信息：{response.json()['msg']}")
+        raise ValueError(f"发送消息卡片时发生错误，错误码：{response.json()['code']}，错误信息：{response.json()['msg']}")
+
+
+def SendTaskSuccessCard(task_name: str, time_cost: int, data_count: int) -> None:
+    time_now = GetNowWithoutMileseconds()
+
+    card = {
+        "header": {
+            "title": {
+                "tag": "plain_text",
+                "content": "采集任务运行成功"
+            },
+            "template": "green"
+        },
+        "elements": [
+            {
+                "tag": "markdown",
+                "content": f"**时间：**{time_now}"
+            },
+            {
+                "tag": "div",
+                "fields": [
+                    {
+                        "is_short": True,
+                        "text": {
+                            "tag": "lark_md",
+                            "content": f"**任务名称**\n{task_name}"
+                        }
+                    },
+                    {
+                        "is_short": False,
+                        "text": {
+                            "tag": "lark_md",
+                            "content": ""
+                        }
+                    },
+                    {
+                        "is_short": True,
+                        "text": {
+                            "tag": "lark_md",
+                            "content": f"**耗时**\n{CostSecondsToString(time_cost)}"
+                        }
+                    },
+                    {
+                        "is_short": True,
+                        "text": {
+                            "tag": "lark_md",
+                            "content": f"**采集数据量**\n{data_count}"
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+
+    SendFeishuCard(card)
