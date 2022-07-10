0e6cb4a9c5013f6f40c652957d558ec57b6ab071
FHU-yezi <yehaowei20060411@qq.com>
2022-07-10 23:36:58 +0800

    feat: 使用线程重新实现数据获取逻辑

--- fetchers/article_FP_rank.py
+++ fetchers/article_FP_rank.py
@@ -1,4 +1,7 @@
-from typing import Generator
+from queue import Queue
+from threading import Thread
+from time import sleep
+from typing import Dict, Generator
 
 from db_manager import GetCollection
 from JianshuResearchTools.convert import ArticleSlugToArticleUrl
@@ -6,7 +9,13 @@ from JianshuResearchTools.rank import GetArticleFPRankData
 from register import TaskFunc
 from utils import GetNowWithoutMileseconds, GetTodayInDatetimeObj
 
+DATA_SAVE_CHECK_INTERVAL = 3
+DATA_SAVE_THRESHOLD = 100
+
 data_collection = GetCollection("article_FP_rank")
+data_queue: "Queue[Dict]" = Queue()
+is_finished = False
+data_count = 0
 
 
 def DataGenerator() -> Generator:
@@ -16,12 +25,9 @@ def DataGenerator() -> Generator:
     return
 
 
-@TaskFunc("简书文章收益排行榜", "0 0 8 1/1 * *")
-def main():
-    start_time = GetNowWithoutMileseconds()
+def DataProcessor() -> None:
+    global is_finished
 
-    temp = []
-    data_count = 0
     for item in DataGenerator():
         if not item["author_name"]:  # 文章被删除导致相关信息无法访问
             data = {
@@ -58,16 +64,43 @@ def main():
                 }
             }
 
-        temp.append(data)
-        data_count += 1
+        data_queue.put(data)
 
-        if len(temp) == 100:
-            data_collection.insert_many(temp)
-            temp.clear()
 
-    if temp:
-        data_collection.insert_many(temp)
-        temp.clear()
+def DataSaver() -> None:
+    global data_count
+
+    while not is_finished:
+        if data_queue.qsize() < DATA_SAVE_THRESHOLD:
+            sleep(DATA_SAVE_CHECK_INTERVAL)
+
+        data_to_save = [data_queue.get()
+                        for _ in range(DATA_SAVE_THRESHOLD)]
+        data_collection.insert_many(data_to_save)
+        data_count += len(data_to_save)
+
+    # 采集完成，存储剩余数据
+    if data_queue.qsize() > 0:
+        data_to_save = [data_queue.get() for _ in range(data_queue.qsize())]
+        data_collection.insert_many(data_to_save)
+        data_count += len(data_to_save)
+
+
+@TaskFunc("简书文章收益排行榜", "0 0 8 1/1 * *")
+def main():
+    global data_count
+    global is_finished
+
+    data_count = 0
+    is_finished = False
+
+    start_time = GetNowWithoutMileseconds()
+
+    saver = Thread(target=DataSaver)
+    saver.start()
+    DataProcessor()
+    is_finished = True
+    saver.join()
 
     stop_time = GetNowWithoutMileseconds()
     cost_time = (stop_time - start_time).total_seconds()
--- fetchers/assets_rank.py
+++ fetchers/assets_rank.py
@@ -1,4 +1,7 @@
-from typing import Generator
+from queue import Queue
+from threading import Thread
+from time import sleep
+from typing import Dict, Generator
 
 from db_manager import GetCollection
 from JianshuResearchTools.convert import UserSlugToUserUrl
@@ -10,7 +13,13 @@ from utils import GetNowWithoutMileseconds, GetTodayInDatetimeObj
 
 set_cache_status(False)
 
+DATA_SAVE_CHECK_INTERVAL = 3
+DATA_SAVE_THRESHOLD = 50
+
 data_collection = GetCollection("assets_rank")
+data_queue: "Queue[Dict]" = Queue()
+is_finished = False
+data_count = 0
 
 
 def DataGenerator(total_count: int) -> Generator:
@@ -24,12 +33,9 @@ def DataGenerator(total_count: int) -> Generator:
                 return
 
 
-@TaskFunc("简书资产排行榜", "0 0 12 1/1 * *")
-def main():
-    start_time = GetNowWithoutMileseconds()
+def DataProcessor() -> None:
+    global is_finished
 
-    temp = []
-    data_count = 0
     for item in DataGenerator(1000):
         if not item["uid"]:  # 用户账号状态异常，相关信息无法获取
             data = {
@@ -65,21 +71,50 @@ def main():
             try:
                 user = User.from_slug(item["uslug"])
                 data["assets"]["FTN"] = user.FTN_count
-                data["assets"]["total"] = round(data["assets"]["FP"] + data["assets"]["FTN"], 3)
+                data["assets"]["total"] = round(
+                    data["assets"]["FP"] + data["assets"]["FTN"], 3
+                )
             except (ResourceError, APIError):
                 # TODO: 记录日志
                 pass
 
-        temp.append(data)
-        data_count += 1
+        data_queue.put(data)
 
-        if len(temp) == 50:
-            data_collection.insert_many(temp)
-            temp.clear()
 
-    if temp:
-        data_collection.insert_many(temp)
-        temp.clear()
+def DataSaver() -> None:
+    global data_count
+
+    while not is_finished:
+        if data_queue.qsize() < DATA_SAVE_THRESHOLD:
+            sleep(DATA_SAVE_CHECK_INTERVAL)
+
+        data_to_save = [data_queue.get()
+                        for _ in range(DATA_SAVE_THRESHOLD)]
+        data_collection.insert_many(data_to_save)
+        data_count += len(data_to_save)
+
+    # 采集完成，存储剩余数据
+    if data_queue.qsize() > 0:
+        data_to_save = [data_queue.get() for _ in range(data_queue.qsize())]
+        data_collection.insert_many(data_to_save)
+        data_count += len(data_to_save)
+
+
+@TaskFunc("简书资产排行榜", "0 0 12 1/1 * *")
+def main():
+    global data_count
+    global is_finished
+
+    data_count = 0
+    is_finished = False
+
+    start_time = GetNowWithoutMileseconds()
+
+    saver = Thread(target=DataSaver)
+    saver.start()
+    DataProcessor()
+    is_finished = True
+    saver.join()
 
     stop_time = GetNowWithoutMileseconds()
     cost_time = (stop_time - start_time).total_seconds()
