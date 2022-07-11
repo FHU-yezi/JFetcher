02e15009cbd6602f5f5fa11c432ac9b0568b409d
FHU-yezi <yehaowei20060411@qq.com>
2022-07-11 20:44:25 +0800

    docs: 补全文档

--- fetchers/__init__.py
+++ fetchers/__init__.py
@@ -2,7 +2,11 @@ from fetchers import (article_FP_rank, assets_rank, daily_update_rank,
                       lottery_data)
 
 
-def init_tasks():
+def init_tasks() -> None:
+    """初始化任务
+
+    也可从此位置通过调用某个模块的 main 函数，手动运行该任务
+    """
     article_FP_rank
     assets_rank
     daily_update_rank
