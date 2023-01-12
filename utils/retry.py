feacd51382c081df7e1d7da4bf6c7f5405b18e26
yezi <yehaowei20060411@qq.com>
2023-01-12 11:34:33 +0800

    feat: 加入超时重试装饰器

---
+++ utils/retry.py
@@ -0,0 +1,28 @@
+from functools import wraps
+
+from backoff import expo, on_exception
+from httpx import TimeoutException
+
+from utils.log import run_logger
+
+
+def retry_on_timeout():
+    def outer(func):
+        @on_exception(
+            expo,
+            TimeoutException,
+            base=2,
+            factor=4,
+            max_tries=5,
+            on_backoff=lambda details: run_logger.warning(
+                f"函数 {func.__name__} 发生超时重试，尝试次数：{details['tries']}，"
+                f"等待时间：{round(details['wait'], 3)}"
+            ),
+        )
+        @wraps(func)
+        def inner(*args, **kwargs):
+            func(*args, **kwargs)
+
+        return inner
+
+    return outer
