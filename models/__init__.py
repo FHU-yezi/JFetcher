ad50b6c963864556711af824d4dd8e99c7033f1e
yezi <yehaowei20060411@qq.com>
2024-02-23 15:39:50 +0800

    feat: 简化 ODM 模型定义方式

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-from typing import List, Type
-
-from beanie import Document
-
-from models.article_earning_rank_record import ArticleEarningRankRecordModel
-
-MODELS: List[Type[Document]] = [ArticleEarningRankRecordModel]
