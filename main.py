d1ca8b1e943da8f660c5059d0e2582ca5b90f7be
yezi <yehaowei20060411@qq.com>
2024-06-24 15:32:18 +0800

    feat: 优化数据库索引创建流程

--- main.py
+++ main.py
@@ -3,33 +3,14 @@ from asyncio import run as asyncio_run
 from prefect import serve
 
 from jobs import DEPLOYMENTS
-from models.jianshu.article_earning_ranking_record import (
-    ArticleEarningRankingRecordDocument,
-)
-from models.jianshu.assets_ranking_record import AssetsRankingRecordDocument
-from models.jianshu.daily_update_ranking_record import DailyUpdateRankingRecordDocument
-from models.jianshu.lottery_win_record import LotteryWinRecordDocument
-from models.jianshu.lp_recommend_article_record import (
-    LPRecommendedArticleRecordDocument,
-)
-from models.jianshu.user import UserDocument as JianshuUserDocument
-from models.jpep.credit_history import CreditHistoryDocument
-from models.jpep.ftn_trade_order import FTNTradeOrderDocument
-from models.jpep.user import UserDocument as JPEPUserDocument
+from models import MODELS
 from utils.log import logger
 
 
 async def main() -> None:
     logger.info("正在为数据库创建索引")
-    await ArticleEarningRankingRecordDocument.ensure_indexes()
-    await AssetsRankingRecordDocument.ensure_indexes()
-    await DailyUpdateRankingRecordDocument.ensure_indexes()
-    await LotteryWinRecordDocument.ensure_indexes()
-    await LPRecommendedArticleRecordDocument.ensure_indexes()
-    await JianshuUserDocument.ensure_indexes()
-    await CreditHistoryDocument.ensure_indexes()
-    await FTNTradeOrderDocument.ensure_indexes()
-    await JPEPUserDocument.ensure_indexes()
+    for model in MODELS:
+        await model.ensure_indexes()
     logger.info("索引创建完成")
 
     logger.info("启动工作流")
