eb53410c1c41f65a7e6064f946f93d3ac5bef639
FHU-yezi <yehaowei20060411@qq.com>
2024-10-31 16:58:36 +0800

    chore: 升级依赖库

--- utils/log.py
+++ utils/log.py
@@ -7,7 +7,7 @@ from utils.config import CONFIG
 logger = Logger(
     display_level=CONFIG.logging.display_level,
     save_level=CONFIG.logging.save_level,
-    connection_string=CONFIG.postgres.logging_connection_string,
+    connection_string=CONFIG.logging.connection_string,
     table=CONFIG.logging.table,
 )
 
