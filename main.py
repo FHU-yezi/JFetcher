3eeb44e8796652a1f4c0fd8e35bc0d2048ceaca1
FHU-yezi <yehaowei20060411@qq.com>
2025-01-21 14:15:45 +0800

    chore: 移除旧版工作流代码

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 from asyncio import run as asyncio_run
 
-from prefect import serve
-
-from jobs import DEPLOYMENTS
-from models import init_db
-from utils.log import logger
-
 
 async def main() -> None:
-    await init_db()
-    logger.info("初始化数据库完成")
-
-    logger.info("启动工作流")
-    await serve(*DEPLOYMENTS, print_starting_message=False)  # type: ignore
+    pass
 
 
 if __name__ == "__main__":
