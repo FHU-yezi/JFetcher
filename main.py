0b202f53cf9e7b80cb215edec4a34168f4df1df1
FHU-yezi <yehaowei20060411@qq.com>
2025-01-22 00:38:06 +0800

    feat: 为工作流部署添加定时

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 from prefect import serve
+from prefect.client.schemas.schedules import CronSchedule
 from prefect.deployments.runner import RunnerDeployment
 
 from flows.jianshu.fetch_article_earning_ranking_data import (
     jianshu_fetch_article_earning_ranking_data,
 )
-from flows.jianshu.fetch_daily_update_ranking import (
+from flows.jianshu.fetch_daily_update_ranking_data import (
     jianshu_fetch_daily_update_ranking_data,
 )
 
 DEPLOYMENTS: tuple[RunnerDeployment, ...] = (
     jianshu_fetch_article_earning_ranking_data.to_deployment(
         name="JFetcher_采集简书文章收益排行榜数据",
+        schedules=(
+            CronSchedule(
+                cron="45 0 * * *",
+                timezone="Asia/Shanghai",
+            ),
+        ),
     ),
     jianshu_fetch_daily_update_ranking_data.to_deployment(
         name="JFetcher_采集简书日更排行榜数据",
+        schedules=(
+            CronSchedule(
+                cron="0 1 * * *",
+                timezone="Asia/Shanghai",
+            ),
+        ),
     ),
-) # type: ignore
+)  # type: ignore
 
 serve(*DEPLOYMENTS)
